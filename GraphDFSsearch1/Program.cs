27198d4b465e9b019e3268e8c74d8bb08c455d33
Frank Braker <frankbraker@gmail.com>
2018-11-12 19:13:38 -0700

    C# graph example
    trying to follow some HackerRank videos on graph search - those examples are Java, but I'm writing this in C#.
    
    Initial commit just defines a Node and a NodeList used for adjacents/neighbors.

---
+++ GraphDFSsearch1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDFSsearch1
+{
+    public class NodeList<T> : Collection<Node<T>>
+    {
+        public NodeList() : base() { }
+
+        public NodeList(int initialSize)
+        {
+            // Add the specified number of items
+            for (int i = 0; i < initialSize; i++)
+                base.Items.Add(default(Node<T>));
+        }
+
+        public Node<T> FindByValue(T value)
+        {
+            // search the list for the value
+            foreach (Node<T> node in Items)
+                if (node.Value.Equals(value))
+                    return node;
+
+            // if we reached here, we didn't find a matching node
+            return null;
+        }
+    }
+
+    public class Node<T>
+    {
+        // Private member-variables
+        private T data;
+        private NodeList<T> neighbors = null;
+
+        public Node() { }
+        public Node(T data) : this(data, null) { }
+        public Node(T data, NodeList<T> neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+
+        protected NodeList<T> Neighbors
+        {
+            get
+            {
+                return neighbors;
+            }
+            set
+            {
+                neighbors = value;
+            }
+        }
+    }
+
+
+    class Graph
+    {
+        // don't worry about the hash table to start with
+        // private Hashtable<int, LinkedListNode> nodeLookup = new Hashtable<int, LinkedListNode>();
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
